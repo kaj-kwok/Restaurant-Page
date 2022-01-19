b3a67e0a670f9a216a8732d4476232293a32bffa
Ka Chun Kwok <kaj.kwok@gmail.com>
2022-01-19 15:06:18 -0800

    created nav element buttons

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import {displayHeader, displayFooter} from "./header/header"
 import createHome from "./home/home"
 import './style.css'
 
+//create Nav
+const nav_bar = document.createElement("div");
+nav_bar.classList.add("nav_bar");
+nav_bar.innerHTML = `<ul>
+    <li>Home</li>
+    <li>Menu</li>
+    <li>About Us</li>
+    `
+
+
 const initialPage = document.querySelector('#content');
 const header = displayHeader("From Land to Sea");
 initialPage.appendChild(header);
+header.appendChild(nav_bar);
 const home = createHome();
 initialPage.appendChild(home);
 initialPage.appendChild(displayFooter())
 
 
-//create Nav
-const nav_bar = document.createElement("div");
-nav_bar.classList.add(nav_bar);
+
